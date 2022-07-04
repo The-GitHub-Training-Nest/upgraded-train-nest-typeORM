9b96cf438947d0f38d155cfb4a6697209faf0c14
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 10:57:12 -0300

    :construction: in progress

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post, Delete, Put } from '@nestjs/common';
 
-@Controller('courses')
+// Body: src/courses/courses.controller.ts -> Configura o objeto com os dados do corpo da requisição;
+// conter dados de um objeto; enviar dados de um formulário com 4, 5 informações que vão estar dentro de um objeto
+
+@Controller('courses') // Endpoint (courses)
 export class CoursesController {
 
   @Get('')
   findAll(): string {
-    return 'This action returns all courses';
+    return `This action returns all courses`;
   }
 
   @Get('/:id')
-  findOne(id: string): string {
+  findOne(@Param(`id`) id): string {
     return `This action returns a #${id} course`;
   }
+
+  @Post('')
+  create(@Body() body): string {
+    return body;
+  }
+
+  @Delete('')
+  delete(): string {
+    return `This action deletes a course`;
+  }
+
+  @Put('')
+  update(): string {
+    return `This action updates a course`;
+  }
+  
 
 }
