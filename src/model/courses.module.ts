f9d632d097258079b28e745920d24177670b1528
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 16:12:58 -0300

    :construction: in progress

--- src/model/courses.module.ts
+++ src/model/courses.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { CoursesController } from '../courses/courses.controller';
-import { CoursesService } from '../courses/courses.service';
+import { CoursesController } from '../controller/courses.controller';
+import { CoursesService } from '../service/courses.service';
 
 @Module({
   imports: [],
