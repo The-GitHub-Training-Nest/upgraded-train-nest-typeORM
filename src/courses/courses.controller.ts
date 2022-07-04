a63cd7a9ee7408b750ce968ac2702196381e6137
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 11:09:26 -0300

    :construction: in progress

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Param, Body, Get, Post, Delete, Put } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post, Delete, Put, HttpCode, HttpStatus } from '@nestjs/common';
 
 // Body: src/courses/courses.controller.ts -> Configura o objeto com os dados do corpo da requisição;
 // conter dados de um objeto; enviar dados de um formulário com 4, 5 informações que vão estar dentro de um objeto
@@ -12,11 +12,14 @@ export class CoursesController {
   }
 
   @Get('/:id')
+  @HttpCode(HttpStatus.OK)
   findOne(@Param(`id`) id): string {
     return `This action returns a #${id} course`;
   }
 
-  @Post('')
+  @Post('') // 201 -> OK que foi criado com sucesso a informação;
+  // Estático;
+  @HttpCode(HttpStatus.NO_CONTENT) // Foi criado, mas ele não consomeu nenhum dado que foi retornado; Nenhum conteúdo no retorno;
   create(@Body('name') body): string {
     return body;
   }
@@ -30,6 +33,7 @@ export class CoursesController {
   update(): string {
     return `This action updates a course`;
   }
-  
 
 }
+
+// O Nest envia resposta para requisições GET bem sucedidas com o código 200 (OK) e para requisições que falharem com o código 400 (Bad Request)
