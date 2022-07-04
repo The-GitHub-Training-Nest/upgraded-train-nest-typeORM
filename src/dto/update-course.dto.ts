dcdc6a0629d70021440aa43fa583796f6a28dacb
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 15:59:45 -0300

    :construction: in progress

--- src/dto/update-course.dto.ts
+++ src/dto/update-course.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types"; // O que a gente precisa validar no UpdateCourseDto é parte do que tem no CreateCourseDto;
 // Basta eu validar no CreaaaateCourseDto e que no UpdateCourseDto vai estar validado;
 // Fica mais fácil para manutenção; Otimização para manutenção;
-import { IsNotEmpty, IsString } from "class-validator";
+// import { IsNotEmpty, IsString } from "class-validator";
 import { CreateCourseDto } from "./create-course.dto";
 
 export class UpdateCourseDto extends PartialType(CreateCourseDto){
