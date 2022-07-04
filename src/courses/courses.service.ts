adef5281a8d8f43e1b3bf0d8a50737357a5eaf5a
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 11:41:44 -0300

    :construction: in progress

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from 'src/entities/course.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  private courses: Course[] = [
+    { id: 1, name: 'course1', description: 'course1 description' },
+  ];
+}
