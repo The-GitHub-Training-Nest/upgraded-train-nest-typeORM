02d59801ed56ba8f6cbc7779f5bb7170c19d0020
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 10:12:27 -0300

    :construction: in progress

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+
+  @Get('')
+  findAll(): string {
+    return 'This action returns all courses';
+  }
+
+  @Get('/:id')
+  findOne(id: string): string {
+    return `This action returns a #${id} course`;
+  }
+
+}
