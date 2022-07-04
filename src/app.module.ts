7d92e37b9cdbc45815ad9fd5f70a04106fd1b098
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 13:59:03 -0300

    :construction: in progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesController } from './courses/courses.controller';
-import { CoursesService } from './courses/courses.service';
-import { ModelModule } from './model/model.module';
+// import { CoursesController } from './courses/courses.controller';
+// import { CoursesService } from './courses/courses.service';
+import { CoursesModule } from './model/courses.module';
 
 @Module({
-  imports: [ModelModule],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
+  imports: [CoursesModule],
+  // controllers: [AppController, CoursesController],
+  controllers: [AppController],
+
+  // providers: [AppService, CoursesService],
+  providers: [AppService],
+
 })
 export class AppModule {}
