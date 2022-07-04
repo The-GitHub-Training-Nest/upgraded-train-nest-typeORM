d86b39d6e783a9676e82662db68fd67d5083cd03
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 16:14:53 -0300

    :truck: removing file

--- src/dto/update-course.dto.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UpdateCourseDto } from './update-course.dto';
-
-describe('UpdateCourseDto', () => {
-  it('should be defined', () => {
-    expect(new UpdateCourseDto()).toBeDefined();
-  });
-});
