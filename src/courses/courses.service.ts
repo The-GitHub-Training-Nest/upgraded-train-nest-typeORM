bbe8fca4ee362b420b3e48f1649cdb0508c5cfb4
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 11:47:11 -0300

    :construction: in progress

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -5,5 +5,7 @@ import { Course } from 'src/entities/course.entity';
 export class CoursesService {
   private courses: Course[] = [
     { id: 1, name: 'course1', description: 'course1 description', tags: ['tag1', 'tag2'] },
+    { id: 2, name: 'course2', description: 'course2 description', tags: ['tag1', 'tag2'] },
+    { id: 3, name: 'course3', description: 'course3 description', tags: ['tag1', 'tag2'] },
   ];
 }
