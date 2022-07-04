12a992f18bbef5f60cb139667be4169bee25ff39
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 15:47:02 -0300

    :construction: in progress

--- src/dto/create-course.dto.ts
+++ src/dto/create-course.dto.ts
@@ -1,6 +1,21 @@
+import { IsNotEmpty, IsString, ValidationOptions } from "class-validator";
+
 export class CreateCourseDto {
+
+  @IsString()
+  @IsNotEmpty()
   readonly id: any;
+
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly tags: string[];
+  
 }
