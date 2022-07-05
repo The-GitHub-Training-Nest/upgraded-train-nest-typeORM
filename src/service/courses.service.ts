a16909d3aee0f539e5e4c5c73b9de74e7b5234ce
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 23:27:15 -0300

    :construction: in progress

--- src/service/courses.service.ts
+++ src/service/courses.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCourseDto } from 'src/dto/create-course.dto';
 import { Course } from 'src/entities/course.entity';
