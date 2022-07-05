8fe3c02edb164572ce0ff7802f51121af2e428b4
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 21:58:41 -0300

    :recycle: refactoring

--- src/model/app.module.ts
+++ src/model/app.module.ts
@@ -5,17 +5,19 @@ import { CoursesModule } from '../model/courses.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoursesModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 49153,
-    username: 'postgres',
-    password: 'postgrespw',
-    database: 'postgres',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    autoLoadEntities: true,
-    synchronize: true,
-  })],
+  imports: [CoursesModule,
+  //   TypeOrmModule.forRoot({
+  //   type: 'postgres',
+  //   host: 'localhost',
+  //   port: 49153,
+  //   username: 'postgres',
+  //   password: 'postgrespw',
+  //   database: 'postgres',
+  //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  //   autoLoadEntities: true,
+  //   synchronize: true,
+  // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 
