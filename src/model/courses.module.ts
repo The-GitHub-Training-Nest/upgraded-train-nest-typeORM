ca7969b52c62c895ddd197d99572435c333e8720
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 22:59:03 -0300

    :construction: in progress

--- src/model/courses.module.ts
+++ src/model/courses.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from 'src/entities/course.entity';
 import { CoursesController } from '../controller/courses.controller';
 import { CoursesService } from '../service/courses.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Course])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
