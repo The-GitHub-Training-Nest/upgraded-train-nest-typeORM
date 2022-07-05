728a743e609a84f897d90324df8f084bfb8ac265
Estevamsl <estevamsouzalaureth@gmail.com>
2022-07-04 22:19:44 -0300

    :construction: in progress

--- src/entities/course.entity.ts
+++ src/entities/course.entity.ts
@@ -1,6 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('courses') // Name da tabela no postgres
 export class Course {
+
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column('json', { nullable: true })
   tags: string[];
 }
